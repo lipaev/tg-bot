04e1ebb7499d322d80528b6b49cf55ee80e18d8e
Lipaev Alexei <alexeijan2@gmail.com>
2026-03-09 11:33:16 +0300

    Improve model loading

--- src/stt/faster_whisper.py
+++ src/stt/faster_whisper.py
@@ -3,26 +3,30 @@ import requests
 from config import config
 from faster_whisper import WhisperModel
 
-model = WhisperModel(model_size_or_path="large-v3", device="cuda", compute_type="int8_float32")
 
-async def speach_to_text(voice_file_id: str) -> str | None:
+def _speech_to_text():
+    model = ""
 
-    file_info = await config.bot.get_file(voice_file_id)
-    file_url = (
-        f"https://api.telegram.org/file/bot{config.bot_token}/{file_info.file_path}"
-    )
+    async def closure(voice_file_id: str) -> str | None:
+        nonlocal model
+        if not model:
+            model = WhisperModel(model_size_or_path="large-v3", device="cuda", compute_type="int8_float32")
+        file_info = await config.bot.get_file(voice_file_id)
+        file_url = (
+            f"https://api.telegram.org/file/bot{config.bot_token}/{file_info.file_path}"
+        )
+        segments, info = model.transcribe(
+            io.BytesIO(requests.get(file_url).content),
+            beam_size=5,
+            # multilingual=False,
+            language="en",
+            without_timestamps=True,
+        )
+        transcript = ""
+        for segment in segments:
+            transcript += segment.text
+        return transcript
 
-    segments, info = model.transcribe(
-        io.BytesIO(requests.get(file_url).content),
-        beam_size=5,
-        # multilingual=False,
-        language="en",
-        without_timestamps=True,
-    )
+    return closure
 
-    transcript = ""
-
-    for segment in segments:
-        transcript += segment.text
-
-    return transcript
+speech_to_text = _speech_to_text()
